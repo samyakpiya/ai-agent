a5842254a4a757fa12a2a8a3bac24c5daa1cda11
Samyak Piya <samyakpiya@gmail.com>
2025-11-07 20:55:48 -0500

    add --verbose flag

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import sys
 
 from dotenv import load_dotenv
 from google import genai
-
+from google.genai import types
 
 def main():
-    if not len(sys.argv) == 2:
+    if not len(sys.argv) >= 2:
         raise Exception("Please provide the prompt as the second argument")
         sys.exit(1)
 
@@ -15,17 +15,23 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    prompt = sys.argv[1]
+    user_prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt
+        contents=messages
     )
 
     print(response.text)
 
     assert response.usage_metadata
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
